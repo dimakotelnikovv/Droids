d1ca1d707f41bb38891f46c9f37814170be88ed2
dimakotelnikovv <dimakotelnikovv@yandex.ru>
2025-11-08 21:35:55 +0300

    Fixed droid5

--- droid5/app/src/main/java/com/example/droid5/MainActivity.kt
+++ droid5/app/src/main/java/com/example/droid5/MainActivity.kt
@@ -2,41 +2,59 @@ package com.example.droid5
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    widthDp = 395,
+    heightDp = 200,
+)
 @Composable
 fun InitialsContainer() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(100.dp)
-            .padding(vertical = 16.dp)
-            .background(Color.Gray),
+            .background(color = Color.Gray),
         contentAlignment = Alignment.Center
     ) {
         Box(
             modifier = Modifier
+                .padding(16.dp)
                 .fillMaxHeight()
-                .width((100-16).dp)
-                .background(Color.Blue, androidx.compose.foundation.shape.CircleShape)
+                .aspectRatio(1f)
+                .background(color = Color.hsv(20f, 1f, 1f), shape = CircleShape),
+            contentAlignment = Alignment.Center
         ) {
             Text(
                 text = "ИКБО-64-23, Котельников Д.Б.",
-                color = Color.White,
-                modifier = Modifier.align(Alignment.Center)
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp
             )
         }
     }
