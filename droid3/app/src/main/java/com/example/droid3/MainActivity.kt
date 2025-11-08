7b5e8c5f7deb872c5968f5829aeb4b67a0d65263
dimakotelnikovv <dimakotelnikovv@yandex.ru>
2025-11-08 20:17:38 +0300

    Fixed droid3

--- droid3/app/src/main/java/com/example/droid3/MainActivity.kt
+++ droid3/app/src/main/java/com/example/droid3/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.droid3.ui.theme.Droid3Theme
 
@@ -60,6 +63,7 @@ fun TextPreview3() {
         modifier = Modifier
             .background(color = Color.Green)
             .fillMaxWidth()
+            .padding(top = 48.dp)
     ) {
         Text(
             text = stringResource(R.string.myString),
