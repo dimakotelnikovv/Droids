6fb86f5180dd992f037089972abbe29f01e62dfe
dimakotelnikovv <dimakotelnikovv@yandex.ru>
2025-11-08 20:11:00 +0300

    Added droid3

---
+++ droid3/app/src/main/java/com/example/droid3/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.droid3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.droid3.ui.theme.Droid3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun TextPreview1() {
+    Text(
+        text = stringResource(R.string.myString),
+        color = Color.Green,
+        fontSize = 16.sp,
+        fontStyle = FontStyle.Italic,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun TextPreview2() {
+    Text(
+        text = stringResource(R.string.myString),
+        color = Color.Black,
+        fontSize = 16.sp,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun TextPreview3() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Green)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.myString),
+            color = Color.Black,
+            fontSize = 24.sp,
+            textDecoration = TextDecoration.Underline
+        )
+    }
+}
